61fe3740100b794b35c000dcf17a8a3767e26211
saeed <bazm586x@gmail.com>
2020-06-04 23:48:04 +0430

    create project

---
+++ src/main/java/ir/hamsarin/accesscontrol/AccessControlApplication.java
@@ -0,0 +1,11 @@
+package ir.hamsarin.accesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessControlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccessControlApplication.class, args);
+    }
+}
